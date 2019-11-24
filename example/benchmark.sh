f31b008dfde3c0b9b74bef58e09131cdabb0b797
prathikshaa-r <pratsr996@gmail.com>
2019-11-24 16:33:59 -0500

    Adding benchmark to project

---
+++ example/benchmark.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#./iozone3_487/src/current/iozone –azcR –b ~/ece566-esa/project/example/exceloutput.xls > logfile
+./iozone3_487/src/current/iozone –azcR –f ~/ece566-esa/project/example/nasdir/testfile –b ~/ece566-esa/project/example/exceloutput.xls > logfile
---
+++ example/nas_mount.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+mount esa05.egr.duke.edu:/home/localadmin/serverShare nasdir/
